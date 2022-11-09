4e73ceb23e44f2dcb946f2c93f3b9378e8c9361f
RafaelZielinski <rafekzielinski@wp.pl>
2022-11-09 19:22:54 +0100

    paging of products

--- src/main/java/pl/zielinski/shop/product/controller/ProductController.java
+++ src/main/java/pl/zielinski/shop/product/controller/ProductController.java
@@ -1,11 +1,12 @@
 package pl.zielinski.shop.product.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.zielinski.shop.product.model.Product;
 import pl.zielinski.shop.product.service.ProductService;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,8 +15,8 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/products")
-    public List<Product> getProducts() {
-        return productService.getProducts();
+    public Page<Product> getProducts(Pageable pageable) {
+        return productService.getProducts(pageable);
     }
 
 
--- src/main/java/pl/zielinski/shop/product/service/ProductService.java
+++ src/main/java/pl/zielinski/shop/product/service/ProductService.java
@@ -1,11 +1,12 @@
 package pl.zielinski.shop.product.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Service;
 import pl.zielinski.shop.product.model.Product;
 import pl.zielinski.shop.product.repository.ProductRepository;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -13,7 +14,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<Product> getProducts() {
-        return productRepository.findAll();
+    public Page<Product> getProducts(@PageableDefault(size = 10) Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 }
