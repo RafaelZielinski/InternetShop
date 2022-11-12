9d1b928dcf1eaa12776c5a8b52ed996888ccef01
RafaelZielinski <rafekzielinski@wp.pl>
2022-11-12 17:09:59 +0100

    adding and updating product

---
+++ src/main/java/pl/zielinski/shop/admin/product/controller/AdminProductController.java
@@ -0,0 +1,49 @@
+package pl.zielinski.shop.admin.product.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+import pl.zielinski.shop.admin.product.controller.dto.AdminProductDto;
+import pl.zielinski.shop.admin.product.model.AdminProduct;
+import pl.zielinski.shop.admin.product.service.AdminProductService;
+
+@RestController
+@RequiredArgsConstructor
+public class AdminProductController {
+
+    private static final Long EMPTY_ID = null;
+    private final AdminProductService adminProductService;
+
+    @GetMapping("/admin/products")
+    public Page<AdminProduct> getProducts(Pageable pageable) {
+        return adminProductService.getProducts(pageable);
+    }
+
+    @GetMapping("/admin/products/{id}")
+    public AdminProduct getProducts(@PathVariable("id") Long id) {
+        return adminProductService.getProducts(id);
+    }
+
+    @PostMapping("/admin/products")
+    public AdminProduct createProduct(@RequestBody AdminProductDto adminProductDto) {
+        return adminProductService.createProduct(mapAdminProduct(adminProductDto, EMPTY_ID));
+    }
+
+    @PutMapping ("/admin/products/{id}")
+    public AdminProduct updateProduct(@RequestBody AdminProductDto adminProductDto, @PathVariable("id") Long id ) {
+        return adminProductService.updateProduct(mapAdminProduct(adminProductDto, id));
+    }
+
+    private AdminProduct mapAdminProduct(AdminProductDto adminProductDto, Long id) {
+        return AdminProduct.builder()
+                .id(id)
+                .name(adminProductDto.getName())
+                .description(adminProductDto.getDescription())
+                .category(adminProductDto.getCategory())
+                .price(adminProductDto.getPrice())
+                .currency(adminProductDto.getCurrency())
+                .build();
+    }
+
+}
---
+++ src/main/java/pl/zielinski/shop/admin/product/service/AdminProductService.java
@@ -0,0 +1,31 @@
+package pl.zielinski.shop.admin.product.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import pl.zielinski.shop.admin.product.model.AdminProduct;
+import pl.zielinski.shop.admin.product.repository.AdminProductRepository;
+
+@Service
+@RequiredArgsConstructor
+public class AdminProductService {
+
+    private final AdminProductRepository adminProductRepository;
+
+    public Page<AdminProduct> getProducts(Pageable pageable) {
+        return adminProductRepository.findAll(pageable);
+    }
+
+    public AdminProduct getProducts(Long id) {
+        return adminProductRepository.findById(id).orElseThrow();
+    }
+
+    public AdminProduct createProduct(AdminProduct product) {
+        return adminProductRepository.save(product);
+    }
+
+    public AdminProduct updateProduct(AdminProduct product) {
+        return adminProductRepository.save(product);
+    }
+}
